31ad8e2e9e4449c765008cf5497d2a3f76610a1f
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-03-31 15:38:31 -0400

    Add paint_it_black iteration

--- js/index.js
+++ js/index.js
@@ -1,27 +1,44 @@
 iterations = [
     {
+        "greyed": true,
         "start_date": "March 1",
-        "end_date": "March 26",
+        "end_date": "March 31",
         "description": "Release Infection Monkey v1.10. We haven't released since August, so this will be one monster of a release.",
         "objectives": [],
         "image": "kraken.jpg"
     },
     {
-        "start_date": "March 29",
-        "end_date": "April 17",
+        "greyed": false,
+        "start_date": "April 1",
+        "end_date": "April 8",
+        "description": "Resolve old PRs and format code with Black.",
+        "objectives": [
+          "Merge report refactoring",
+          "Merge AppImage",
+          "Run black on entire codebase",
+          "Resolve flake8 findings",
+          "Add pre-commit hooks for black, flake8, isort",
+          "CI pipeline uses same black, flake8, and isort rules as pre-commit hooks"
+        ],
+        "image": "paint_it_black.jpg"
+    },
+    {
+        "greyed": false,
+        "start_date": "April 9",
+        "end_date": "April 22",
         "description": "Find creative ways to improve our flow and do more with less.",
         "objectives": [
             "Automated end-to-end tests",
             "Automated build process",
             "AppImage Package",
-            "Expedite release process",
-            "Use pre-commit hooks to improve code quality"
+            "Expedite release process"
         ],
         "image": "more_with_less.jpg"
     },
     {
-        "start_date": "April 20",
-        "end_date": "May 1",
+        "greyed": false,
+        "start_date": "April 23",
+        "end_date": "May 6",
         "description": "Harden Infection Monkey against attack. Analyze code for weak security practices and fix them.",
         "objectives": [
             "Address bandit findings",
@@ -38,13 +55,15 @@ iterations = [
         "image": "hatches.jpg"
     },
     {
-        "start_date": "May 4",
+        "greyed": false,
+        "start_date": "May 7",
         "end_date": "May 29",
         "description": "Add scenarios wizard that helps users configure and run breach & attack simulations quickly.",
         "objectives": [],
         "image": "lightning.jpg"
     },
     {
+        "greyed": false,
         "start_date": "June 1",
         "end_date": "June 17",
         "description": "Add ransomware simulation capability.",
@@ -52,6 +71,7 @@ iterations = [
         "image": "ransom.jpg"
     },
     {
+        "greyed": false,
         "start_date": "June 18",
         "end_date": "June 26",
         "description": "Fix as many bugs as possible, improving automated tests along the way.",
@@ -59,6 +79,7 @@ iterations = [
         "image": "bugs.jpg"
     },
     {
+        "greyed": false,
         "start_date": "June 28",
         "end_date": "June 30",
         "description": "Release Infection Monkey v1.11 with awesome new ransomware and scenarios features.",
@@ -85,11 +106,13 @@ class TimelineRenderer {
 		let side = this.even ? "Left" : "Right";
 		this.even = ! this.even;
 
+    let opacity = iteration.greyed ? "greyed" : "";
+
     return (
       `<div class="timeline-item">
         <div class="timeline-img"></div>
 
-        <div class="timeline-content timeline-card js--fadeIn${side}">
+        <div class="timeline-content timeline-card ${opacity} js--fadeIn${side}">
         	<div class="timeline-img-header">
 				<img class="meme" src="./images/${iteration.image}">
         	</div>
