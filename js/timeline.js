cde49fa3b0816f835de665dbb92c4d031c584f9a
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-01-20 10:57:52 -0500

    Fix tabs -> spaces in timeline.js

--- js/timeline.js
+++ js/timeline.js
@@ -5,18 +5,18 @@ class TimelineRenderer {
     console.log(document.URL)
   }
   
-	render() {
-		let milestonesHTML = '';
+  render() {
+    let milestonesHTML = '';
 
-		for (let i of this.milestones) {
-			milestonesHTML += this.renderMilestoneCard(i);
-		}
-		return milestonesHTML;
+    for (let i of this.milestones) {
+      milestonesHTML += this.renderMilestoneCard(i);
+    }
+    return milestonesHTML;
   }
 
-	renderMilestoneCard(milestone) {
-		let side = this.even ? "Left" : "Right";
-		this.even = ! this.even;
+  renderMilestoneCard(milestone) {
+    let side = this.even ? "Left" : "Right";
+    this.even = ! this.even;
 
     let opacity = milestone.greyed ? "greyed" : "";
 
@@ -25,9 +25,9 @@ class TimelineRenderer {
         <div class="timeline-img"></div>
 
         <div class="timeline-content timeline-card ${opacity} js--fadeIn${side}">
-        	<div class="timeline-img-header">
-				<img class="meme" src="${milestone.image}">
-        	</div>
+          <div class="timeline-img-header">
+        <img class="meme" src="${milestone.image}">
+          </div>
             <div class="date">${milestone.start_date} - ${milestone.end_date}</div>
             <div class="description">
                 <p>${milestone.description}</p>
@@ -38,25 +38,25 @@ class TimelineRenderer {
         </div>
       </div>`
     );
-	}
+  }
 
   renderList(title, items) {
-		if (items.length == 0) {
-			return ""
-		}
+    if (items.length == 0) {
+      return ""
+    }
 
-		let item_list = '                <ul>';
-		for (let o of items) {
-			item_list += `\n                   <li>${o}</li>`;
-		}
-		item_list += '\n                </ul>';
+    let item_list = '                <ul>';
+    for (let o of items) {
+      item_list += `\n                   <li>${o}</li>`;
+    }
+    item_list += '\n                </ul>';
 
-		return("<h3>" + title + "</h3>\n" + item_list);
+    return("<h3>" + title + "</h3>\n" + item_list);
   }
 }
 
 function renderTimeline(milestones) {
-	let renderer = new TimelineRenderer(milestones)
+  let renderer = new TimelineRenderer(milestones)
   document.getElementById("timeline-container").innerHTML = renderer.render();
 }
 
@@ -65,48 +65,48 @@ function fadeIn() {
 
   if ($(window).width() < 768) {
 
-  	if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-  		$('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-  	}
+    if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+      $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+    }
 
-  	sr.reveal('.js--fadeInRight', {
-	    origin: 'right',
-	    distance: '300px',
-	    easing: 'ease-in-out',
-	    duration: 800,
-	  });
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
 
   } else {
-  	
-  	sr.reveal('.js--fadeInLeft', {
-	    origin: 'left',
-	    distance: '300px',
-		  easing: 'ease-in-out',
-	    duration: 800,
-	  });
+    
+    sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
 
-	  sr.reveal('.js--fadeInRight', {
-	    origin: 'right',
-	    distance: '300px',
-	    easing: 'ease-in-out',
-	    duration: 800,
-	  });
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
 
   }
   
   sr.reveal('.js--fadeInLeft', {
-	    origin: 'left',
-	    distance: '300px',
-		  easing: 'ease-in-out',
-	    duration: 800,
-	  });
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
 
-	  sr.reveal('.js--fadeInRight', {
-	    origin: 'right',
-	    distance: '300px',
-	    easing: 'ease-in-out',
-	    duration: 800,
-	  });
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
 }
 
 $(function(){
